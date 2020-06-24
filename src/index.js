4f3300e643aae46558a1a2542d6e3ef1b41e1b5e
hwayn <hwayning@gmail.com>
2020-06-24 18:15:08 +0800

    react start

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import './index.css';
+// import App from './App';
+import styles from './index.module.css'
+import logo from './logo.png'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const name = "react";
+const obj = {
+  name: 'zhangsan',
+  age: 20
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const arr = [0, 1, 2];
+
+function formatUser(user) {
+  return user.name + "-" + user.age
+}
+
+const jsx = (
+  <div>
+    <div>hello, {name}</div>
+    <div>{formatUser(obj)}</div>
+    <ul>
+      {arr.map(item => (
+        <li key={item}>{item}</li>
+      ))}
+    </ul>
+    
+    <div className={styles.content}>
+      <img
+        src={logo} alt=''
+        className={styles.logo}
+        // style={{width: '50px'}}
+      />
+    </div>
+    
+  </div>
+)
+ReactDOM.render(jsx, document.getElementById('root'));
