4d4948b21adbfc2051a4b6b8d04880581689be62
hwayn <hwayning@gmail.com>
2020-06-25 01:01:35 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import TestComponent from './TestComponent'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       react learn
-      <TestComponent />
     </div>
   );
 }
