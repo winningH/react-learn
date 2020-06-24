c77e8333d651906147c4e6983279ffd2a26ec8d5
hwayn <hwayning@gmail.com>
2020-06-25 01:00:17 +0800

    update

--- src/TestComponent/index.js
+++ src/TestComponent/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './index.module.css'
-import logo from '../logo.png'
+import logo from '../assets/logo.png'
 
 function formatUser(user) {
   return user.name + "-" + user.age
