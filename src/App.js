b73fec1e1e46b147f079ed9912b0277fffa547d2
hwayn <hwayning@gmail.com>
2020-06-28 16:38:47 +0800

    react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Redirect, Switch } from 'react-router-dom'
+import HomePage from './router/HomePage';
+import UserPage from './router/UserPage';
+import ErrorPage from './router/ErrorPage';
 // import TestComponent from './TestComponent'
 // import ClassComponent from './pages/ClassComponent'
 // import FunctionComponent from './pages/FunctionComponent'
 // import SetStatePage from './pages/SetStatePage'
-import LifeCyclePage from './pages/LifeCyclePage'
+// import LifeCyclePage from './pages/LifeCyclePage'
 // import ReduxPage from './pages/ReduxPage'
-import ReactReduxPage from './pages/ReactReduxPage'
+// import ReactReduxPage from './pages/ReactReduxPage'
 
 function App() {
   return (
@@ -16,9 +20,25 @@ function App() {
       {/* <ClassComponent /> */}
       {/* <FunctionComponent /> */}
       {/* <SetStatePage /> */}
-      <LifeCyclePage />
+      {/* <LifeCyclePage /> */}
       {/* <ReduxPage /> */}
-      <ReactReduxPage />
+      {/* <ReactReduxPage /> */}
+
+      <Router>
+        <Link to="/home">首页</Link>
+        <Link to="/user">用户中心</Link>
+        <Switch>
+          {/* route 有三种写法 children > component > render。不能同时出现*/}
+          <Route exact path="/" render={() => 
+            <Redirect to="/home"></Redirect>
+          }>
+          </Route>
+          <Route path="/home" component={HomePage}></Route>
+          <Route path="/user" component={UserPage}></Route>
+          <Route component={ErrorPage}></Route>
+        </Switch>
+        
+      </Router>
     </div >
   );
 }
