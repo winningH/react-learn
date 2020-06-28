08756e2861e255944424c5046dc142dddda3cb39
hwayn <hwayning@gmail.com>
2020-06-28 22:04:51 +0800

    hoc: 高阶组件

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import ContextPage from './pages/ContextPage'
+// import ContextPage from './pages/ContextPage'
+import HocPage from './pages/HocPage'
 
 function App() {
   return (
     <div className="App">
-      <ContextPage />
+      {/* <ContextPage /> */}
+      <HocPage></HocPage>
     </div>
   );
 }
